bea29dba6d025d937d1b4fe72a015db38ee37e6d
SmartSecurity <alphaplusteam2016@gmail.com>
2016-10-28 19:48:35 +0330

    Fixed !

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -1,5 +1,3 @@
-do
-
 function run(msg, matches)
   return "‰—Œ «‘ —«ò —»«  ¬·›« Å·«”\n1- „«Â«‰Â 3,500  Ê„«‰\n2- ‰«„ÕœÊœ 20,000  Ê„«‰\n3- ê—ÊÂ Â«Ì »«·« 100 ‰›— „Ì Ê«‰‰œ 10 —Ê“  ”  œ—Ì«›  ò‰‰œ !\n »—«Ì Œ—Ìœ Ì« „‘«Ê—Â =>https://telegram.me/joinchat/DHWOSj_pSnSRmusPS1P_Cw"
 end
@@ -14,9 +12,8 @@ return {
    "^/Nerkh$",
    "^!Nerkh$",
    "^Nerkh$",
-   "^‰—Œ$",
+   "^‰—Œ$"
 
   },
   run = run
 }
-end
