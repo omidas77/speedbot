441c0afd8e54d9ee789be4dd2bbfd20e02aef095
SmartSecurity <alphaplusteam2016@gmail.com>
2016-10-28 19:30:40 +0330

    Fixed !

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -1,15 +1,11 @@
  	local function run(msg, matches)
 	local text = matches[1]
 	local b = 1
-	if not is_momod(msg) then
-    return 
-	end
 	while b ~= 0 do
 	text = text:trim()
 	text,b = text:gsub('^!+','')
 	end
 	return text
-	end
 	
 	return {
 	description = "Reply Your Sent Message",
