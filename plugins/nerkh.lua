f3be5a0900a422b500c3fe222bb8c8e6c063ac92
SmartSecurity <alphaplusteam2016@gmail.com>
2016-10-28 19:45:33 +0330

    add nerkh

---
+++ plugins/nerkh.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return "‰—Œ «‘ —«ò —»«  ¬·›« Å·«”\n1- „«Â«‰Â 3,500  Ê„«‰\n2- ‰«„ÕœÊœ 20,000  Ê„«‰\n3- ê—ÊÂ Â«Ì »«·« 100 ‰›— „Ì Ê«‰‰œ 10 —Ê“  ”  œ—Ì«›  ò‰‰œ !\n »—«Ì Œ—Ìœ Ì« „‘«Ê—Â =>https://telegram.me/joinchat/DHWOSj_pSnSRmusPS1P_Cw"
+end
+return {
+  description = "Nerkh Sakht Group", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/nerkh$",
+    "^!nerkh$",
+    "^nerkh$",
+    "^nerkh$",
+   "^/Nerkh$",
+   "^!Nerkh$",
+   "^Nerkh$",
+   "^‰—Œ$",
+
+  },
+  run = run
+}
+end
